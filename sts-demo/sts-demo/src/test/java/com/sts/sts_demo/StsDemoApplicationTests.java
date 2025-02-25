90927f05e64eba50a560db94f1140cad5a59f7ff
Faris Mohammed K C <farismhd954@gmail.com>
2025-02-25 17:12:56 +0530

    STS project1

---
+++ sts-demo/sts-demo/src/test/java/com/sts/sts_demo/StsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts.sts_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
